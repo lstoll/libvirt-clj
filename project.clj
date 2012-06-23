b23dbd3f34a2b25872c8eb54d3663f45e1250f61
Lincoln Stoll <lstoll@lstoll.net>
2012-06-23 21:46:40 +0200

    Depends on jna

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject libvirt-clj "0.1.0-SNAPSHOT"
   :description "libvirt clojure bindings"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.lstoll/libvirt "0.4.7.1"]])
+                 [org.clojars.lstoll/libvirt "0.4.7.1"]
+                 [net.java.dev.jna/jna "3.0.9"]])
