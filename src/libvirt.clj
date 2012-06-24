65e3dd1a7304c2121a85728779e3d736c187d09d
Lincoln Stoll <lstoll@lstoll.net>
2012-06-24 19:01:49 +0200

    Proper domain list, create from XML

--- src/libvirt.clj
+++ src/libvirt.clj
@@ -6,7 +6,18 @@
   [uri & {:keys [ro] :or {ro false}}]
   (Connect. uri ro))
 
-(defn list-domains
-  "Lists domains for the connection"
+(defn domain-list
+  "Gets list of Domains - returns their IDs"
   [conn]
   (seq (.listDomains conn)))
+
+(defn domains
+  "Gets domains for the connection"
+  [conn]
+  (map #(.domainLookupByID conn %) (domain-list conn)))
+
+(defn create-domain
+  "Creates a domain from the given XML config format. Will automatically start it"
+  [conn domain-xml]
+  ;; Just use default flags
+  (.domainCreateXML conn domain-xml 0))
--- test/libvirt_clj/test/libvirt.clj
+++ test/libvirt_clj/test/libvirt.clj
@@ -3,9 +3,35 @@
   (:use [clojure.test]))
 
 (def turi "test:///default")
+(defn tc [] (conn turi))
+(def new-domain-xml
+"<domain type='test'>
+  <name>testnew</name>
+  <memory unit='KiB'>8388608</memory>
+  <currentMemory unit='KiB'>2097152</currentMemory>
+  <vcpu>2</vcpu>
+  <os>
+    <type arch='i686'>hvm</type>
+    <boot dev='hd'/>
+  </os>
+  <clock offset='utc'/>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>destroy</on_crash>
+  <devices>
+  </devices>
+</domain>")
 
 (deftest conn-returns-conn
   (is org.libvirt.Connect (type (conn turi))))
 
-(deftest domain-list
-  (is 1 (count (list-domains (conn turi)))))
+(deftest domain-list-test
+  (is 1 (count (domain-list (tc)))))
+
+(deftest domains-test
+  (is org.libvirt.Domain (first (domains (tc)))))
+
+(deftest create-domain-test
+  (let [new (create-domain (tc) new-domain-xml)]
+    (is org.libvirt.Domain (type new))
+    (is "testnew" (.getName new))))
