a68ce9447b38c2b68d74aaa0fa2af4f4085858e3
Lincoln Stoll <lstoll@lstoll.net>
2012-07-04 00:10:55 +0200

    Import connection, nodeinfo, domain and network tests from java
    This gives us a solid baseline of tests.

--- test/libvirt_clj/test/libvirt_test.clj
+++ test/libvirt_clj/test/libvirt_test.clj
@@ -2,39 +2,43 @@
   (:use [libvirt])
   (:use [clojure.test]))
 
-(def turi "test:///default")
-(defn tc [] (conn turi))
-(def new-domain-xml
-"<domain type='test'>
-  <name>testnew</name>
-  <memory unit='KiB'>8388608</memory>
-  <currentMemory unit='KiB'>2097152</currentMemory>
-  <vcpu>2</vcpu>
-  <os>
-    <type arch='i686'>hvm</type>
-    <boot dev='hd'/>
-  </os>
-  <clock offset='utc'/>
-  <on_poweroff>destroy</on_poweroff>
-  <on_reboot>restart</on_reboot>
-  <on_crash>destroy</on_crash>
-  <devices>
-  </devices>
-</domain>")
+(deftest connection-tests
+  (let [c (connect "test:///default")]
+    (is (= "Test" (:type c)) "getType")
+    (is (= (:URI c) "test:///default") "getURI")
+    (is (= (max-vcpus c "xen") 32) "getMaxVcpus(xen)")
+    (is (not (nil? (:host-name c))) "getHostName()")
+    (is (not (nil? (:capabilities c))) "getCapabilities()")
+    ;; (is (> 6000 (:connection-version c)) "getConnectionVersion()")
+    (is (not (:encrypted c)) "isEncrypted()")
+    ))
 
-(deftest conn-returns-conn
-  (is org.libvirt.Connect (type (conn turi))))
+(deftest node-info-tests
+  (let [c (connect "test:///default")
+        ni (node-info c)]
+    (is (= "i686" (:model ni)) "nodeInfo.model")
+    (is (= 3145728 (:memory ni)) "nodeInfo.memory")
+    (is (= 2 (:cores ni)) "nodeInfo.cores")
+    ))
 
-(deftest bad-conn-throws
-  (is (thrown? org.libvirt.LibvirtException (conn "test:///defaultBROKEN"))))
+(deftest network-create-test
+  (let [c (connect "test:///default")
+        network-1-xml "<network><name>createst</name><uuid>004b96e1-2d78-c30f-5aa5-f03c87d21e68</uuid><bridge name='createst'/><forward dev='eth0'/><ip address='192.168.66.1' netmask='255.255.255.0'><dhcp><range start='192.168.66.128' end='192.168.66.253'/></dhcp></ip></network>"
+        network-2-xml "<network><name>deftest</name><uuid>004b96e1-2d78-c30f-5aa5-f03c87d21e67</uuid><bridge name='deftest'/><forward dev='eth0'/><ip address='192.168.88.1' netmask='255.255.255.0'><dhcp><range start='192.168.88.128' end='192.168.88.253'/></dhcp></ip></network>"
+        network-1 (network-create c network-1-xml)
+        network-2 (network-define c network-2-xml)]
+    (is (= 2 (:num-of-networks c)) "Number of Networks")
+    (is (= 1 (:num-of-defined-networks c)) "Number of Defined Networks")
+    (is (:active network-1) "Network 1 should be active")
+    (is (not (:active network-2)) "Network 2 should not be active")
+    ))
 
-(deftest domain-list-test
-  (is 1 (count (domain-list (tc)))))
-
-(deftest domains-test
-  (is "test" (:name (first (domains (tc))))))
-
-(deftest create-domain-test
-  (let [new (create-domain (tc) new-domain-xml)]
-    (is org.libvirt.Domain (type (:o new)))
-    (is "testnew" (:name new))))
+(deftest domain-create-test
+  (let [c (connect "test:///default")
+        domain-1-xml "<domain type='test' id='2'><name>deftest</name><uuid>004b96e1-2d78-c30f-5aa5-f03c87d21e70</uuid><memory>8388608</memory><vcpu>2</vcpu><os><type arch='i686'>hvm</type></os><on_reboot>restart</on_reboot><on_poweroff>destroy</on_poweroff><on_crash>restart</on_crash></domain>"
+        domain-2-xml "<domain type='test' id='3'><name>createst</name><uuid>004b96e1-2d78-c30f-5aa5-f03c87d21e67</uuid><memory>8388608</memory><vcpu>2</vcpu><os><type arch='i686'>hvm</type></os><on_reboot>restart</on_reboot><on_poweroff>destroy</on_poweroff><on_crash>restart</on_crash></domain>"
+        domain-1 (define-domain c domain-1-xml)
+        domain-2 (create-linux-domain c domain-1-xml 0)]
+    (is (= 2 (:num-of-domains c)) "Number of Domains")
+    (is (:active domain-1) "Domain1 should not be active")
+    ))
