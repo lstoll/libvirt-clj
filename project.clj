8efc64b8a376cc67d4399059a03846f920af9e51
Lincoln Stoll <lstoll@lstoll.net>
2012-07-03 07:11:25 +0200

    Update libvirt-java

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject libvirt-clj "0.1.0-SNAPSHOT"
   :description "libvirt clojure bindings"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.lstoll/libvirt "0.4.7.1"]
+                 [org.clojars.lstoll/libvirt "0.4.7.2"]
                  [net.java.dev.jna/jna "3.0.9"]])
