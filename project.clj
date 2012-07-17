fa8a64dc6b8a119a0c2817dd05fe35c7d9ccf48f
Lincoln Stoll <lstoll@lstoll.net>
2012-07-17 17:09:01 +0200

    rel 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject libvirt-clj "0.1.0-SNAPSHOT"
+(defproject libvirt-clj "0.1.0"
   :description "libvirt clojure bindings"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.lstoll/libvirt "0.4.7.2"]
