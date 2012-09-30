9e216e9387d790f2244826688899c2927d757bc0
Lincoln Stoll <lstoll@me.com>
2012-09-30 14:16:37 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject libvirt-clj "0.1.0"
+(defproject libvirt-clj "0.1.1"
   :description "libvirt clojure bindings"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.lstoll/libvirt "0.4.7.2"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojars.lstoll/libvirt "0.4.7.3"]
                  [net.java.dev.jna/jna "3.0.9"]]
   :java-source-path "java")
