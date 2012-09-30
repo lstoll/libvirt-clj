7b4faef164613c563f20cb054b1c26408a973ad8
Lincoln Stoll <lstoll@me.com>
2012-09-30 14:19:59 +0200

    Upstreamed this class

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "libvirt clojure bindings"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojars.lstoll/libvirt "0.4.7.3"]
-                 [net.java.dev.jna/jna "3.0.9"]]
-  :java-source-path "java")
+                 [net.java.dev.jna/jna "3.0.9"]])
